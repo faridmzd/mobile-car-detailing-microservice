6fc6e82ff4d5968638b844a955c3eca7459112df
Farid <faridbusiness@bk.ru>
2023-08-13 22:10:59 +0200

    added mapping profiles

---
+++ src/Services/Client/src/Application/Common/Mappings/CommandToDomainProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Client.Application.Customers.Commands;
+using Client.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Application.Common.Mappings
+{
+    public class CommandToDomainProfile : Profile
+    {
+        public CommandToDomainProfile()
+        {
+            CreateMap<CreateCustomerCommand, Customer>();
+
+        }
+    }
+}
---
+++ src/Services/Client/src/Application/Common/Mappings/DomainToResponseProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Client.Application.Customers.Responses;
+using Client.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Application.Common.Mappings
+{
+    public class DomainToResponseProfile : Profile
+    {
+        public DomainToResponseProfile() 
+        {
+            CreateMap<Customer, CreateCustomerResponse>();
+        }
+    }
+}
