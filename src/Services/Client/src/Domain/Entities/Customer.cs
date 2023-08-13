7705b711eabb073796eaa6d26f9d60c39b7eb67e
Farid <faridbusiness@bk.ru>
2023-08-13 22:09:26 +0200

    added customer entity

--- src/Services/Client/src/Domain/Entities/Customer.cs
+++ src/Services/Client/src/Domain/Entities/Customer.cs
@@ -1,14 +1,14 @@
 ﻿using Client.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Client.Domain.Entities
 {
     public class Customer : BaseEntity
     {
-        public string  Name { get; set; } 
+        public string Name { get; set; } = null!;
+
+        public Customer()
+        {
+        }
     }
 }
