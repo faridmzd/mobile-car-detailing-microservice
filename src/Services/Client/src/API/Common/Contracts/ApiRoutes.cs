41eef3efc8ee614a304614baa0b0a543aeee4439
Farid <faridbusiness@bk.ru>
2023-08-16 18:18:33 +0200

    added api routes

---
+++ src/Services/Client/src/API/Common/Contracts/ApiRoutes.cs
@@ -0,0 +1,18 @@
+﻿namespace Client.API.Common.Contracts
+{
+    public static class ApiRoutes
+    {
+        private const string Root = "api";
+        private const string Version = "v1";
+        private const string Base = Root + "/" + Version;
+
+        public static class Note
+        {
+            public const string GetAll = Base + "/customer";
+            public const string Get = Base + "/customer/{id:Guid}";
+            public const string Add = Base + "/customer";
+            public const string Update = Base + "/customer/{id:Guid}";
+            public const string Delete = Base + "/customer/{id:Guid}";
+        }
+    }
+}
