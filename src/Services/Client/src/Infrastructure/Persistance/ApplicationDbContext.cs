f84ffbe0a4272549d7f6a6c43fbb26902b0e5b61
Farid <faridbusiness@bk.ru>
2023-08-18 11:32:00 +0200

    added abstract app db contect

---
+++ src/Services/Client/src/Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Client.Application.Common.Interfaces;
+using Client.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
