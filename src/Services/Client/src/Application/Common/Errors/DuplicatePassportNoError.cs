340a09d58e9ee8cb9d1ce6494052c4b54697b955
Farid <faridbusiness@bk.ru>
2023-08-18 11:00:46 +0200

    added custom error

--- src/Services/Client/src/Application/Common/Errors/DuplicatePassportNoError.cs
+++ src/Services/Client/src/Application/Common/Errors/DuplicatePassportNoError.cs
@@ -1,18 +1,21 @@
-﻿using FluentResults;
+﻿using Client.Application.Common.HTTP;
+using FluentResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client.Application.Common.Errors
 {
-   public class DuplicatePassportNoError : IError
+   public class DuplicatePassportNoError : Error
     {
-        public List<IError> Reasons => new();
-
-        public string Message => $"Customer with given PassportNo already exists";
-
-        public Dictionary<string, object> Metadata => new();
+        public DuplicatePassportNoError(Guid Id)
+        {
+            Message = $"Client with passport number : {Id} already exists";
+                
+            WithMetadata(HttpContextItemKeys.StatusCode, HttpStatusCode.Forbidden);
+        }
     }
 }
