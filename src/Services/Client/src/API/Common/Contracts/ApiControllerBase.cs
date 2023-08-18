f20d78883f9072f26e12e870f7031a499e9b2b9f
Farid <faridbusiness@bk.ru>
2023-08-18 11:00:02 +0200

    added controller base class

---
+++ src/Services/Client/src/API/Common/Contracts/ApiControllerBase.cs
@@ -0,0 +1,31 @@
+﻿using Client.Application.Common.HTTP;
+using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Client.API.Common.Contracts
+{
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender? _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+
+        public IActionResult Problem(List<IError> errors)
+        {
+            var firstError = errors.First();
+          
+                return (IActionResult)Results.Problem(
+
+                    statusCode: firstError.HasMetadataKey(HttpContextItemKeys.StatusCode)
+                    ? (int)(firstError.Metadata[HttpContextItemKeys.StatusCode]) : (int)HttpStatusCode.Conflict,
+
+                    title: firstError.Message
+                    );
+
+            
+        }
+    }
+}
