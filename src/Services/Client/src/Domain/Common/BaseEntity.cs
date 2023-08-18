31d5fcf0a8fbaf53defcfa0720e6af80f09ec66e
Farid <faridbusiness@bk.ru>
2023-08-18 11:05:38 +0200

    addde db atrribites

--- src/Services/Client/src/Domain/Common/BaseEntity.cs
+++ src/Services/Client/src/Domain/Common/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,13 @@ namespace Client.Domain.Common
 {
     public abstract class BaseEntity
     {
+        [Key]
         public Guid Id { get; set; }
 
+        [Column("CREATED_AT")]
         public DateTime CreatedAt { get; set;}
 
+        [Column("UPDATED_AT")]
         public DateTime UpdatedAt { get; set;}
     }
 }
--- src/Services/Client/src/Domain/Entities/Customer.cs
+++ src/Services/Client/src/Domain/Entities/Customer.cs
@@ -1,12 +1,14 @@
 ﻿using Client.Domain.Common;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Client.Domain.Entities
 {
     public class Customer : BaseEntity
     {
+        [Column("PASSPORT_NO")]
         public string PassportNo { get; set; } = null!;
 
+        [Column("NAME")]
         public string Name { get; set; } = null!;
 
        
