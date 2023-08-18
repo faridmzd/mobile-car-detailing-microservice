8ae75c14e4f9772b3d42c2786e7cfa6e21b8c685
Farid <faridbusiness@bk.ru>
2023-08-18 11:38:59 +0200

    added dependency injection extensions package for registering validation dependencies

--- src/Services/Client/src/Application/ConfigureServices.cs
+++ src/Services/Client/src/Application/ConfigureServices.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using FluentValidation;
+
 
 namespace Microsoft.Extensions.DependencyInjection555;
 
@@ -10,7 +12,7 @@ namespace Microsoft.Extensions.DependencyInjection555;
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(cfg => {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); });
 
