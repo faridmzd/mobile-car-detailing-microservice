c4fb455174e54995a3ae3bf18482fab9f578ee54
Farid <faridbusiness@bk.ru>
2023-08-18 11:32:30 +0200

    added extension method for configuring inf services

---
+++ src/Services/Client/src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,29 @@
+﻿using Client.Application.Common.Interfaces;
+using Client.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("MSSQL"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+
+
+    }
+}
