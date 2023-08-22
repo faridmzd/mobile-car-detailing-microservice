0a9528134ec4d0ffee1c902f5dfd3da033685b19
Farid <faridbusiness@bk.ru>
2023-08-22 04:07:39 +0200

    added controller methods

--- src/Services/Client/src/API/Controllers/ClientController.cs
+++ src/Services/Client/src/API/Controllers/ClientController.cs
@@ -2,6 +2,7 @@
 using Client.Application.Customers.Commands;
 using Client.Application.Customers.Queries;
 using FluentResults;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Client.API.Controllers
@@ -19,14 +20,42 @@ namespace Client.API.Controllers
         }
 
         [HttpGet(ApiRoutes.Customer.Get)]
-        public string Get(int id)
+        public async Task<IActionResult> Get([FromRoute] Guid id)
         {
-            return "value";
+            var result = await Mediator.Send(new GetCustomerQuery(id));
+
+            return result switch
+            {
+                { IsSuccess: true } => Ok(result.Value),
+                { IsFailed: true } => Problem(result.Errors),
+                _ => null!
+            };
+
         }
 
         [HttpPost(ApiRoutes.Customer.Add)]
         public async Task<IActionResult> Post([FromBody] CreateCustomerCommand command)
         {
+
+            var result = await Mediator.Send(command);
+
+
+
+
+            return result switch
+            {
+                { IsSuccess: true } => Ok(result.Value),
+                { IsFailed: true } => Problem(result.Errors),
+                _ => null!
+            };
+
+        }
+
+        [HttpPut(ApiRoutes.Customer.Update)]
+        public async Task<IActionResult> Put([FromRoute] Guid id, [FromBody] UpdateCustomerCommandRequest request)
+        {
+            var command = new UpdateCustomerCommand(id, request.PassportNo, request.Name);
+
             var result = await Mediator.Send(command);
 
             return result switch
@@ -34,20 +63,21 @@ namespace Client.API.Controllers
                 { IsSuccess: true } => Ok(result.Value),
                 { IsFailed: true } => Problem(result.Errors),
                 _ => null!
-            }; 
-
-        }
-
-        [HttpPut(ApiRoutes.Customer.Update)]
-        public void Put(int id, [FromBody] string value)
-        {
-
-
+            };
         }
 
         [HttpDelete(ApiRoutes.Customer.Delete)]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
+            var result = await Mediator.Send(new DeleteCustomerCommand(id)); ;
+
+            return result switch
+            {
+                { IsSuccess: true } => NoContent(),
+                { IsFailed: true } => Problem(result.Errors),
+                _ => null!
+            };
+
         }
     }
 }
---
+++ src/Services/Client/src/Application/Customers/Commands/UpdateCustomerCommand.cs
@@ -0,0 +1,28 @@
+﻿using Client.Application.Customers.Responses;
+using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Application.Customers.Commands
+{
+    public record UpdateCustomerCommand(string PassportNo, string Name) : IRequest<Result<UpdateCustomerCommandResponse>>
+    {
+
+        public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand,Result<UpdateCustomerCommandResponse>>
+        {
+
+            public async Task<Result<UpdateCustomerCommandResponse>> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+            {
+               
+
+
+
+            }
+        }
+
+    }
+}
